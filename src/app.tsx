31f578e1317b90de3e220b09d6b5f83cb56564f6
Anders Bech Mellson <anbme@energinet.dk>
2024-07-23 23:27:42 +0200

    Sky 1.0 init

--- src/app.tsx
+++ src/app.tsx
@@ -1,64 +1,22 @@
-import { useEffect, useState } from 'react';
-import Counter from './examples/counter';
-import TrafficLight from './examples/trafficLight/trafficLight';
-import Home from './home';
-
-const pages = ['home', 'counter', 'trafficlight'] as const;
-type Page = (typeof pages)[number];
-
-function getComponent(page: Page) {
-  switch (page) {
-    case 'home':
-      return <Home />;
-    case 'counter':
-      return <Counter />;
-    case 'trafficlight':
-      return <TrafficLight />;
-  }
-}
-
-function getPage() {
-  if (typeof window !== 'undefined') {
-    const queryParams = new URLSearchParams(window.location.search);
-    const pageQueryParam = queryParams.get('page');
-    if (pageQueryParam && pages.includes(pageQueryParam as Page)) {
-      return pageQueryParam as Page;
-    }
-  }
-  return 'home';
-}
-
-function setPageQueryParam(page: Page) {
-  if (typeof window !== 'undefined') {
-    window.history.replaceState(
-      null,
-      '',
-      `?page=${page}` + window.location.hash,
-    );
-  }
-}
+import { createSkyClient } from '@statelyai/sky';
 
 export default function App() {
-  const [page, setPage] = useState<Page>(getPage());
-  useEffect(() => setPageQueryParam(page), [page]);
+  const sky = createSkyClient({
+    apiKey: import.meta.env.VITE_SKY_API_KEY,
+  });
+
+  // const subscription = sky.subscribeTo('sky', (state) => {
+  //   console.log('Sky state updated:', state);
+  // });
 
   return (
     <div className="app">
       <div className="app-header">
         <p className="sky-header">Stately Sky Starter ⛅</p>
-        <div className="app-pages">
-          {pages.map((p) => (
-            <button
-              key={p}
-              className={p === page ? 'app-active-page' : 'app-page'}
-              onClick={() => setPage(p)}
-            >
-              {p}
-            </button>
-          ))}
-        </div>
+        <p className="app-description">
+          A starter template for building React applications with Sky.
+        </p>
       </div>
-      {getComponent(page)}
     </div>
   );
 }
--- src/examples/trafficLight/Light.tsx
+++
@@ -1,9 +0,0 @@
-export function Light({
-  color,
-  isActive,
-}: {
-  color: string;
-  isActive: boolean;
-}) {
-  return <div className={`light ${color} ${isActive ? 'active' : ''}`}></div>;
-}
